d3af2e890fb2ba54511a4eb7feeadf28578dcf9c
Gihyun Kim <83570546+kimgihean@users.noreply.github.com>
2022-06-03 16:04:53 +0900

    Update OpenSource_12_김서영_김부경_김기현_김원종.py

--- OpenSource_12_김서영_김부경_김기현_김원종.py
+++ OpenSource_12_김서영_김부경_김기현_김원종.py
@@ -1,3 +1,12 @@
+"""
+'Eccomerce Shipping Data'
+
+This file is finding best score function after preprocessing yourself
+    Algorithm Method
+        Clissifing  : Decision Tree, KNN, LogisticRegresison
+        Regression  : Polynomial Regression, Multiple Regression
+        Ensemble    : Random Forest, Gradient Boosting
+"""
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
@@ -16,6 +25,57 @@ import time
 
 
 def autoML(data, X, y):
+    """
+    This function automatically combines and executes 
+        Scaling, Modeling, and Evaluation, 
+    and returns 
+        the result in the type of a DataFrame.
+
+    Args:
+        data (DataFrame) : Data in the type of a DataFrame after removing dirty data
+                          , The target data is binary classification.
+        X (Training dataset)
+        y (Target dataset)
+
+    Returns:
+        Data frame : DataFrame with columns 
+                    'Model', 
+                    'Scaler', 
+                    'Parameter_dict', 
+                    'score_cv', 
+                    'score_auc', 
+                    'score_ac', 
+                    'score_r2', 
+                    'runtime', 
+                    'model_'.
+                    
+    Example:
+    >>> import OpenSource_12_김서영_김부경_김기현_김원종 as test
+    >>> data.autoML()
+    >>> <function OpenSource_12_김서영_김부경_김기현_김원종.autoML(data, X, y)>
+    >>> data.autoML(DataFrame, Training, Test)
+    >>> Model            Scaler                        Parameter_dict  \
+92   RandomForest  StandardScaler()   {'n_estimators': 5, 'max_depth': 3}   
+145  RandomForest    MinMaxScaler()   {'n_estimators': 8, 'max_depth': 4}   
+157  RandomForest    MinMaxScaler()  {'n_estimators': 11, 'max_depth': 4}   
+188  RandomForest    RobustScaler()   {'n_estimators': 9, 'max_depth': 3}   
+108  RandomForest  StandardScaler()   {'n_estimators': 9, 'max_depth': 3}   
+
+     score_cv  score_auc  score_ac score_r2   runtime  \
+92   0.690480   0.737257  0.685152           0.021019   
+145  0.688402   0.732980  0.685152           0.034000   
+157  0.688142   0.723200  0.682424           0.042003   
+188  0.687752   0.719793  0.683939           0.032011   
+108  0.687493   0.736067  0.686061           0.034000   
+
+                                                model_  
+92   (DecisionTreeClassifier(max_depth=3, max_featu...  
+145  (DecisionTreeClassifier(max_depth=4, max_featu...  
+157  (DecisionTreeClassifier(max_depth=4, max_featu...  
+188  (DecisionTreeClassifier(max_depth=3, max_featu...  
+108  (DecisionTreeClassifier(max_depth=3, max_featu...  
+    """
+    
     scalers = [StandardScaler(), MinMaxScaler(), RobustScaler()]
     regression_name = ['polynomialRegression', 'multipleRegression']
     classification_name = ['DecisionTree(gini)', 'DecisionTree(entropy)', 'KNeighborsClassifier', 'LogisticRegression']
@@ -190,9 +250,46 @@ def autoML(data, X, y):
     
 
 def best_n(df, ft_name, n):
+    """
+    This function automatically Find best score
+    and returns DataFrame after descending sorting.
+
+    Args:
+        df (DataFrame)      : Final result(final_result) after all process
+        ft_name (String)    : What kinds of scoring (cv, auc, ac, r2)
+        n (integer)         : The highest rank in order n (1 ~ n)
+
+    Returns:
+        Data frame
+    """
     df.sort_values(by = ft_name, ascending = False, inplace = True)
     return df[:n]
     
+
+"""
+[ The Feature ]
+ID                  : ID Number of Customers
+Warehouse block     : The Company have big Warehouse which is divided in to block 
+                      such as A, B, C, D, F.
+Mode of shipment    : The Company Ships the products in multiple way such as Ship, Flight and Road.
+Customer care calls : The number of calls made from enquiry for enquiry of the shipment.
+Customer rating     : The company has rated from every customer. 
+                      1 is the lowest (Worst), 5 is the highest (Best).
+Cost of the product : Cost of the Product in US Dollars
+Prior purchases     : The Number of Prior Purchase
+Product importance  : The company has categorized the product in the various parameter 
+                      such as low, medium, high
+Gender              : Male and Female
+Discount offered    : Discount offered on that specific product
+Weight in gms       : It is the weight in grams
+Reached on time     : It is the target variable, 
+                      where 1 Indicates that the product has NOT reached on time
+                      and 0 indicates it has reached on time
+
+[ The Target Feature ]
+Reached on time
+"""
+
     
 # main code
 data = pd.read_csv("/ecommerce_shipping.csv")
